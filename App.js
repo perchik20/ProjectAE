12311d54e9c5ce034201bfc0b353728839d0f621
perchik20 <104394329+perchik20@users.noreply.github.com>
2022-11-03 02:57:46 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import User from './pages/userp';
+
+export default function App() {
+  return (
+    <View>
+      <User/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
